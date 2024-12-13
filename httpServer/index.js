942a52c755492b1cee6bdc9062e9e3fb6271eb9f
adityamahla <adityamahla@gmail.com>
2024-12-13 11:20:28 +1100

    URL req, res and query parameters

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,13 +1,30 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 const myServer = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") {
+    return res.end();
+  }
   console.log("New request received");
-  const log = `${Date.now()} New Request Received\n`;
-  fs.appendFile("./test.txt", log, (err, data) => {
-    res.end("Hello from Server Again");
-  });
+  const log = `${Date.now()} ${req.url} New Request Received\n`; //install npm i url package to parse the url
+  const myUrl = url.parse(req.url, true);
 
-  res.end("Hello from server");
+  fs.appendFile("./test.txt", log, (err, data) => {
+    switch (myUrl.pathname) {
+      case "/":
+        res.end(`Hello from home`);
+        break;
+      case "/about":
+        res.end(`Hello ${myUrl.query.name}`);
+        break;
+      case "/search":
+        res.end(`Serach results for ${myUrl.query.search_query}`);
+        break;
+      default:
+        res.end("404 not found");
+        break;
+    }
+  });
 });
 
 myServer.listen(8000, () => console.log("Server Started!"));
