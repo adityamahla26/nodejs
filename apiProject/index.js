85ed7fba8756d55d05f194ca122bf1476a2ffb63
adityamahla <adityamahla@gmail.com>
2024-12-20 09:59:25 +1100

    Implement REST API Using CRUD operations on json file

--- apiProject/index.js
+++ apiProject/index.js
@@ -2,41 +2,75 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
-const users = require("./MOCK_DATA.json")
+let users = require("./MOCK_DATA.json");
+const fs = require("fs");
 
-app.get("/users",(req,res)=>{
-    const html = `
+//Middleware plugin urlencoded
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/users", (req, res) => {
+  const html = `
     <ul>
-    ${users.map(({id,first_name,last_name,email,gender,job_title})=>`<li key=${id}>${first_name}</li>`).join("")}
+    ${users.map(({ id, first_name, last_name, email, gender, job_title }) => `<li>${first_name}</li>`).join("")}
     </ul>
-    `
-    res.send(html);
-})
+    `;
+  res.send(html);
+});
 
-app.get("/api/users",(req,res)=>{
-    return res.json(users)
-})
+app
+  .route("/api/users")
+  .get((req, res) => {
+    return res.json(users);
+  })
+  .post((req, res) => {
+    const id = users.length + 1;
+    const body = req.body;
+    const user = { id, ...body };
+    users.push(user);
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+    return res.json(user);
+  });
 
-app.get("/api/users/:id",(req,res)=>{
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
     const id = Number(req.params.id);
-    const user = users.find((user)=>user.id === id);
-    res.json(user);
-})
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    const id = Number(req.params.id);
+    const { first_name, last_name, email, gender, job_title } = req.body;
+    const userIndex = users.findIndex((user) => user.id === id);
+    users[userIndex].first_name = first_name;
+    users[userIndex].last_name = last_name;
+    users[userIndex].email = email;
+    users[userIndex].gender = gender;
+    users[userIndex].job_title = job_title;
 
-app.post("/api/users/create",(req,res)=>{
-    res.send("New user created")
-})
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
 
-app.patch("/api/users/:id",(req,res)=>{
-    const id  = req.params.id;
-    res.send(`user ${id} updated`)
-})
+    return res.send(`user ${id} updated`);
+  })
+  .delete((req, res) => {
+    const id = Number(req.params.id);
+    users = users.filter((user) => user.id !== id);
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+    return res.send(`uer ${id} deleted`);
+  });
 
-app.delete("/api/users/:id",(req,res)=>{
-    const id = req.params.id;
-    res.send(`uer ${id} deleted`)
-})
-
-app.listen(port,()=>{
-    console.log(`app is listening at port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`app is listening at port ${port}`);
+});
