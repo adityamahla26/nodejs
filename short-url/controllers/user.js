d65538b62b27c6bca4773be89908addacefb5ec6
Adi Mahla <adityamahla@gmail.com>
2025-01-14 09:18:08 +1100

    stateless token based authentication

--- short-url/controllers/user.js
+++ short-url/controllers/user.js
@@ -16,8 +16,10 @@ export async function handleUserLogIn(req, res) {
   if (!user) {
     return res.render("login", { error: "Invalid username or password" });
   }
-  const sessionId = uuidv4();
-  setUser(sessionId, user);
-  res.cookie("uid", sessionId);
+  //const sessionId = uuidv4();
+  //setUser(sessionId, user);
+  //res.cookie("uid", sessionId);
+  const token = setUser(user);
+  res.cookie("uid", token);
   return res.redirect("/");
 }
--- short-url/service/auth.js
+++ short-url/service/auth.js
@@ -1,9 +1,23 @@
-const sessionIdToUserMap = new Map();
+import jwt from "jsonwebtoken";
+//const sessionIdToUserMap = new Map();
+// export function setUser(id, user) {
+//   sessionIdToUserMap.set(id, user);
+// }
 
-export function setUser(id, user) {
-  sessionIdToUserMap.set(id, user);
+// export function getUser(id) {
+//   return sessionIdToUserMap.get(id);
+// }
+
+const secret = "abc";
+
+export function setUser(user) {
+  console.log(user);
+  return jwt.sign({ _id: user._id.toString(), ...user }, secret);
 }
 
-export function getUser(id) {
-  return sessionIdToUserMap.get(id);
+export function getUser(token) {
+  if (!token) {
+    return null;
+  }
+  return jwt.verify(token, secret);
 }
